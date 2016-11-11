282a550ab73daa32a80e2cfecfb924e08e3ea3b9
joec <joseph.cargill@imanage.com>
2016-11-11 13:00:31 +0000

    first commit

---
+++ README.txt
@@ -0,0 +1,14 @@
+qubmc_web README
+==================
+
+Getting Started
+---------------
+
+- cd <directory containing this file>
+
+- $VENV/bin/pip install -e .
+
+- $VENV/bin/initialize_qubmc_web_db development.ini
+
+- $VENV/bin/pserve development.ini
+
---
+++ qubmc_web/models/user.py
@@ -0,0 +1,27 @@
+import datetime 
+from qubmc_web.models.meta import Base
+from passlib.apps import custom_app_context as blogger_pwd_context
+from sqlalchemy import (
+    Column,
+    Integer,
+    Unicode,
+    UnicodeText,
+    DateTime,
+)
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(Unicode(255), unique=True, nullable=False)
+    password = Column(Unicode(255), nullable=False)
+    last_logged = Column(DateTime, default=datetime.datetime.utcnow)
+    
+    def verify_password(self, password):
+        # is it cleartext?
+        if password == self.password:
+            self.set_password(password)
+        return blogger_pwd_context.verify(password, self.password)
+        
+    def set_password(self, password):
+        password_hash = blogger_pwd_context.encrypt(password)
+        self.password = password_hash
